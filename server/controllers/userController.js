ac8a8865470362d42188b36c3947a23485dd27f5
Samantha Salley <samanthasalley@gmail.com>
2017-12-05 10:52:27 -0800

    update syntax

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,7 @@ const UserController = {};
 * @description - creates a new User and saves to DB
 */
 UserController.addNewUser = (req, res, next) => {
-  if (!req.body.userId) return res.status(400).send({err: 'Invalid request'});
+  if (!req.body.userId) return res.status(400).send({ err: 'Invalid request' });
 
   const newUser = new User(req.body.userId);
 
@@ -22,7 +22,7 @@ UserController.addNewUser = (req, res, next) => {
   db.conn.one(query)
     .then(createdUser => {
       res.status(200).send({
-        'msg' : 'user successfully created',
+        'msg': 'user successfully created',
         'uid': createdUser.user_id
       })
     })
@@ -36,7 +36,7 @@ UserController.addNewUser = (req, res, next) => {
 *                returns 400 if user is not found.
 */
 UserController.getOneUser = (req, res, next) => {
-  if (!req.params.id) return res.status(400).send({err: 'Invalid request'});
+  if (!req.params.id) return res.status(400).send({ err: 'Invalid request' });
 
   const query = {
     text: "SELECT * FROM \"Users\" WHERE user_id=$1",
@@ -51,15 +51,34 @@ UserController.getOneUser = (req, res, next) => {
 
 /**
 * Update User
+* @param {id} - required, varchar, unique - should match a user ID in table
+* @argument - optional, varchar - equates to the most recent post id 
+*             associated with the user.  Used when getting post metadata.
+* @description - updates user row in Users table if matching user_id is found.
+*                returns updated user info.
 */
 UserController.updateOneUser = (req, res, next) => {
+  if (!req.params.id) return res.status(400).send({ err: 'Invalid request' });
+  
+  const updatedUser = new User(req.params.id, req.body.recent_post_id);
 
+  const query = {
+    text: "UPDATE \"Users\" SET recent_post_id=$2 WHERE user_id=$1 RETURNING *",
+    values: Object.values(updatedUser)
+  };
+
+  db.conn.one(query)
+    .then(updatedUser => res.status(200).send(updatedUser))
+    .catch(err => res.status(400).send(err));
 };
 
 /**
 * Remove User
+* @param {id} - required, varchar, unique - should match a user ID in table
+* @description - deletes user row in Users table if matching user_id is found.
 */
 UserController.removeOneUser = (req, res, next) => {
+  if (!req.params.id) return res.status(400).send({ err: 'Invalid request' });
 
   const query = {
     text: "DELETE FROM \"Users\" WHERE user_id=$1",
@@ -69,7 +88,7 @@ UserController.removeOneUser = (req, res, next) => {
   db.conn.any(query)
     .then(deletedUser => {
       res.status(200).send({
-        'msg' : 'user successfully deleted'
+        'msg': 'user successfully deleted'
       })
     })
     .catch(err => res.status(404).send(err));
